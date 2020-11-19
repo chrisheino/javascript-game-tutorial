8d8eb83f54a7da34404260c4b88e9581a6e5b174
chrisheino <cheino@juno.com>
2020-11-18 22:13:55 -0800

    Second step in initial part of the tutorial.  Also integrated JSHint (4 options not working).

--- JavaScriptGameTutorial/WebContent/game.js
+++ JavaScriptGameTutorial/WebContent/game.js
@@ -1,5 +1,13 @@
-var canvas = document.getElementById("game-layer");
-var context = canvas.getContext("2d");
+/* globals document: false */
+const canvas = document.getElementById("game-layer");
+const context = canvas.getContext("2d");
 
 context.fillStyle = "grey";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = "red";
+context.fillRect(5, 5, 10, 15);
+context.fillStyle = "blue";
+context.fillRect(25, 25, 20, 20);
+context.fillStyle = "green";
+context.fillRect(50, 50, 20, 40);
